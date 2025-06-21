0dde559d7aab32e01bae205bf1ccbfc69ee60906
miwanoff <marina.iva.noffa@gmail.com>
2025-06-21 15:57:15 +0300

    Add weather output

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,36 +10,87 @@ const api = {
   icon: "https://openweathermap.org/img/w",
 };
 
-function Image(props) {
-  return <img src={props.src} alt="logo" style={{ width: "100px" }} />;
-}
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
 
-function Header(props) {
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  return (
+    <div className="container">
+      <Header className="alert alert-warning mt-4 p-5 rounded text-center" />
+      <div className="row">
+        <div className="col-md-4 col-sm-12 my-3">
+          Today: {moment().format("dddd")} {moment().format("LL")}
+        </div>
+      </div>
+      <div className="row">
+        <div className="search-panel col-sm-12 col-md-4 my-3">
+          <input
+            type="text"
+            className="form-control search-input"
+            placeholder="Search"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyDown={search}
+          />
+        </div>
+      </div>
+      {typeof weather.main != "undefined" ? (
+        <div className="row">
+          <div className="search-panel col-md-4 col-sm-12 my-3">
+            <div className="card">
+              <div className="card-header bg-primary text-white">
+                <h2>
+                  {console.log(weather)}
+                  {weather.name}, {weather.sys.country}
+                </h2>
+              </div>
+              <div className="card-body bg-warning">
+                <div className="temp">
+                  <h3>{Math.round(weather.main.temp)}°c</h3>
+                </div>
+                <div className="weather">{weather.weather[0].main}</div>
+                <p className="card-text">{weather.weather[0].description}</p>
+                <div id="icon">
+                  <img
+                    src={`${api.icon}/${weather.weather[0].icon}.png`}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
+    </div>
+  );
+};
+
+const Image = (props) => {
+  return <img src={props.src} alt="logo" style={{ width: "100px" }} />;
+};
+
+const Header = (props) => {
   return (
     <div className={props.className}>
       <Image src={logo} />
       <h1>Weather</h1>
     </div>
   );
-}
-
-function App() {
-  return (
-  <div className="container">
-    <Header className="alert bg-warning" />
-    <div className="row">
-      <div className="search-panel col-sm-12 col-md-4 my-3">
-        <input
-          type="text"
-          className="form-control search-input"
-          placeholder="Search"
-        />
-      </div>
-    </div>
-  </div>
-  );
-}
-
-
+};
 
 export default App;
